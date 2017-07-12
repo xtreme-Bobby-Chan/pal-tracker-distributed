630cff1b4d9140d134c1d6fd8a86ad16a74ee402
Pivotal - Dev Floater 70 <pivotal@Dev-Floater-70.local>
2017-07-12 14:04:47 -0400

    Added Eureka

---
+++ platform-services/eureka-server/src/main/java/io/pivotal/pal/tracker/eurekaserver/App.java
@@ -0,0 +1,16 @@
+package io.pivotal.pal.tracker.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaClient
+@EnableEurekaServer
+@SpringBootApplication
+public class App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
