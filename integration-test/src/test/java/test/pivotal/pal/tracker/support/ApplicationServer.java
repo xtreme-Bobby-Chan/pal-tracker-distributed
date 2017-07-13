20d6c36f3b75a279ac9fcbd368e526b564a015db
Pivotal - Dev Floater 70 <pivotal@Dev-Floater-70.local>
2017-07-13 11:17:15 -0400

    Added pipkin

--- integration-test/src/test/java/test/pivotal/pal/tracker/support/ApplicationServer.java
+++ integration-test/src/test/java/test/pivotal/pal/tracker/support/ApplicationServer.java
@@ -23,8 +23,8 @@ public class ApplicationServer {
 
     public void start(Map<String, String> env) throws IOException, InterruptedException {
         ProcessBuilder processBuilder = new ProcessBuilder()
-            .command("java", "-jar", jarPath)
-            .inheritIO();
+                .command("java", "-jar", jarPath)
+                .inheritIO();
 
         processBuilder.environment().put("SERVER_PORT", port);
         env.forEach((key, value) -> processBuilder.environment().put(key, value));
@@ -36,12 +36,13 @@ public class ApplicationServer {
         String dbUrl = "jdbc:mysql://localhost:3306/" + dbName + "?useSSL=false&useTimezone=true&serverTimezone=UTC&useLegacyDatetimeCode=false";
 
         start(envMapBuilder()
-            .put("SPRING_DATASOURCE_URL", dbUrl)
-            .put("EUREKA_CLIENT_ENABLED", "false")
-            .put("RIBBON_EUREKA_ENABLED", "false")
-            .put("REGISTRATION_SERVER_RIBBON_LISTOFSERVERS", "http://localhost:8883")
-            .put("APPLICATION_OAUTH_ENABLED", "false")
-            .build()
+                .put("SPRING_DATASOURCE_URL", dbUrl)
+                .put("EUREKA_CLIENT_ENABLED", "false")
+                .put("RIBBON_EUREKA_ENABLED", "false")
+                .put("REGISTRATION_SERVER_RIBBON_LISTOFSERVERS", "http://localhost:8883")
+                .put("APPLICATION_OAUTH_ENABLED", "false")
+                .put("SPRING_ZIPKIN_ENABLED", "false")
+                .build()
         );
     }
 
